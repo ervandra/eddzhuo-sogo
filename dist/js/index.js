84f862dfdbb64cff58cfca83419d8b329a875821
Ervandra Halim <ervandra.halim@gmail.com>
2018-10-17 15:45:11 +0700

    Init login page

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,4 +3,12 @@
 var jQvan = jQuery.noConflict();
 jQvan(document).ready(function ($) {
   $('body').addClass('ready');
+  $('.dashboard-toggle, .dashboard-wrapper, .close-dashboard-menu a').on('click', function (e) {
+    e.preventDefault();
+    if ($('.dashboard-menu').hasClass('show')) {
+      $('.dashboard-wrapper, .dashboard-menu').removeClass('show');
+    } else {
+      $('.dashboard-wrapper, .dashboard-menu').addClass('show');
+    }
+  });
 });
